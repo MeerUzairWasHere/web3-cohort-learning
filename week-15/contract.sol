5d9ab0a493df1f08d910a91903cddacfd1832786
Meer Uzair <meer.uxair007@gmail.com>
2025-07-07 21:12:26 +0530

    updated

--- week-15/contract.sol
+++ week-15/contract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 // Solidity version (^0.8.0 specified above)
 
 // Inheritance - Importing Ownable from OpenZeppelin
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
 // External contract interface
 interface IExternalContract {
